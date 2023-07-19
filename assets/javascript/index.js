131c145c2dd5f5cef68e53481c990ec7d489e825
Picasso <urvishp13@gmail.com>
2023-07-19 13:49:05 -0400

    did the metric to imperial conversion

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,13 +9,13 @@ convertBtn.addEventListener("click", convert(input.value))
 
 // convert(amount):
 function convert(amount) {
-    // take the amount and use it to do an Metric conversion
+    // take the amount and use it to do an Imperial to Metric conversion (i.e. get metric amount)
     metric(amount)
-    // take the amount and use it to do a Imperial conversion
-    // imperial(amount)
+    // take the amount and use it to do a Metric to Imperial conversion (i.e. get imperial amount)
+    imperial(amount)
 }
 
-// metric:
+// metric(amount):
 function metric(amount) {
     const feet = document.querySelector(".length .metric")
     const gallons = document.querySelector(".volume .metric")
@@ -32,5 +32,27 @@ function metric(amount) {
         // liters --> gals
         gallons.textContent = `${amount} liters = ${(amount * lTOgal).toFixed(3)} gallons`
         // kilograms --> lbs
-        pounds.textContent = `${amount} kilograms = ${(amount * kgTOlb).toFixed(3)} pounds`
+        pounds.textContent = `${amount} kilos = ${(amount * kgTOlb).toFixed(3)} pounds`
+}
+
+// imperial(amount):
+function imperial(amount) {
+    const meters = document.querySelector(".length .imperial")
+    const liters = document.querySelector(".volume .imperial")
+    const kilograms = document.querySelector(".mass .imperial")
+
+    // conversions
+    const ftTOm = 1/3.281
+    const galTOl = 1/0.264
+    const lbTOkg = 1/2.204
+
+    console.log(ftTOm)
+
+    // take amount and convert and write to DOM in:
+        // meters --> ft
+        meters.textContent = `${amount} feet = ${(amount * ftTOm).toFixed(3)} meters`
+        // liters --> gals
+        liters.textContent = `${amount} gallons = ${(amount * galTOl).toFixed(3)} liters`
+        // kilograms --> lbs
+        kilograms.textContent = `${amount} pounds = ${(amount * lbTOkg).toFixed(3)} kilos`
 }
