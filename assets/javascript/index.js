f2a5aeccf32af2ef9020679b7935992019a9402b
Picasso <urvishp13@gmail.com>
2023-07-19 12:48:57 -0400

    did the imperial to metric conversion

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,11 +9,28 @@ convertBtn.addEventListener("click", convert(input.value))
 
 // convert(amount):
 function convert(amount) {
-    // take the amount and use it to do an Imperial conversion
-    imperial(amount)
-    // take the amount and use it to do a Metric conversion
+    // take the amount and use it to do an Metric conversion
     metric(amount)
+    // take the amount and use it to do a Imperial conversion
+    // imperial(amount)
 }
 
-// imperial:
-    //
+// metric:
+function metric(amount) {
+    const feet = document.querySelector(".length .metric")
+    const gallons = document.querySelector(".volume .metric")
+    const pounds = document.querySelector(".mass .metric")
+
+    // conversions
+    const mTOft = 3.281
+    const lTOgal = 0.264
+    const kgTOlb = 2.204
+
+    // take amount and convert and write to DOM in:
+        // meters --> ft
+        feet.textContent = `${amount} meters = ${(amount * mTOft).toFixed(3)} feet`
+        // liters --> gals
+        gallons.textContent = `${amount} liters = ${(amount * lTOgal).toFixed(3)} gallons`
+        // kilograms --> lbs
+        pounds.textContent = `${amount} kilograms = ${(amount * kgTOlb).toFixed(3)} pounds`
+}
